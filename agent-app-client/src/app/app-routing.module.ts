5061eaadc9fd288b15304ede6cb115ea0db209ed
anagrahovac <grahovac.ana99@gmail.com>
2022-05-28 19:41:36 +0200

    feat: add companies page

--- agent-app-client/src/app/app-routing.module.ts
+++ agent-app-client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CompaniesComponent } from './component/companies/companies.component';
 
 const routes: Routes = [
+  { path: '', component: CompaniesComponent },
   { path: 'companies', component: CompaniesComponent }
 ];
 
--- agent-app-client/src/app/component/companies/companies.component.ts
+++ agent-app-client/src/app/component/companies/companies.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./companies.component.css']
 })
 export class CompaniesComponent implements OnInit {
-
+  text: string = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio."
   constructor() { }
 
   ngOnInit(): void {
