f24bd71d2f30f41ec97a2872f655ccdd16b8d7a4
anagrahovac <grahovac.ana99@gmail.com>
2022-05-29 04:10:41 +0200

    feat: add company page with comments

--- agent-app-client/src/app/app-routing.module.ts
+++ agent-app-client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompaniesComponent } from './component/companies/companies.component';
+import { CompanyComponent } from './component/company/company.component';
 
 const routes: Routes = [
   { path: '', component: CompaniesComponent },
-  { path: 'companies', component: CompaniesComponent }
+  { path: 'companies', component: CompaniesComponent },
+  { path: 'company/id', component: CompanyComponent }
 ];
 
 @NgModule({
--- agent-app-client/src/app/app.module.ts
+++ agent-app-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -12,22 +13,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { CompaniesComponent } from './component/companies/companies.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatTabsModule } from '@angular/material/tabs';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { CompaniesComponent } from './component/companies/companies.component';
+import { CompanyComponent } from './component/company/company.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CompaniesComponent
+    CompaniesComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
@@ -46,6 +49,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatDividerModule,
     MatAutocompleteModule,
     MatChipsModule,
+    MatTabsModule,
     FormsModule,
     ReactiveFormsModule
     
